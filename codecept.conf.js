b5fff5b80ca3801c308669626b9b086e575640c0
Rom4ig <romses2015@gmail.com>
2020-01-23 19:12:48 +0300

    added package.json

---
+++ codecept.conf.js
@@ -0,0 +1,34 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// HEADLESS=true npx codecept run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: 'test/*-spec.js',
+  output: './output',
+  helpers: {
+    WebDriver: {
+      url: 'https://www.tut.by/',
+      browser: 'chrome'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'codecept',
+  plugins: {
+    retryFailedStep: {
+      enabled: true
+    },
+    wdio: {
+      enabled: true,
+      services: ['selenium-standalone']
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+};
