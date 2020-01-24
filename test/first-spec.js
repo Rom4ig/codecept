a67a58c29887cb71bf7b4ebce2bacd1ce0586cff
Rom4ig <romses2015@gmail.com>
2020-01-24 10:55:44 +0300

    Added axios test

--- test/first-spec.js
+++ test/first-spec.js
@@ -1,4 +1,5 @@
 const expect = require('chai').expect;
+const axios = require('axios');
 
 Feature('UI and API test');
 Scenario('UI test tut.by. Success sign in', (I) => {
@@ -10,8 +11,21 @@ Scenario('UI test tut.by. Success sign in', (I) => {
     I.see('Роман Грунковский');
 });
 
-Scenario('Api test https://reqres.in/. Get API/USERS', async(I) => {
+Scenario('Api test https://reqres.in/. Get API/USERS by helpers codeceptJS', async (I) => {
     const res = await I.sendGetRequest('api/users');
     console.log(res.data);
     expect(res.data.data[0].email).to.eql('george.bluth@reqres.in');
 });
+Scenario('Api test https://reqres.in/. Get API/USERS by axios', () => {
+// Make a request for a users
+    axios.get('https://reqres.in/api/users')
+        .then(function (res) {
+            // handle success
+            console.log(res.data);
+            expect(res.data.data[1].email).to.eql('janet.weaver@reqres.in')
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
+})
