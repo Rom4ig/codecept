4a9378656833a8a1e655c4dd60b41e9bbe236fa4
Rom4ig <romses2015@gmail.com>
2020-01-27 19:26:58 +0300

    Added allure

--- README.md
+++ README.md
@@ -7,7 +7,8 @@
 ### author: Rom4ik
 ## requirements
 ### Default browser - chrome
-### use ==> npm install ==> to download all packages
-### use ==> npm test -- --browser=browserName (chrome is default) in root.
+### use ==> "npm install" ==> to download all packages
+### use ==> "npm test -- [test/fileName-spec.js] (all files default) [--browser=browserName]  (chrome is default)" ==> to start test.
+### use ==> "allure serve output" ==> to open allure report
 ## Info
 ### Products test work only in fullscreen mode!!!(Browser run in fullscreen mode(default))
--- codecept.conf.js
+++ codecept.conf.js
@@ -1,40 +1,43 @@
-const { setHeadlessWhen } = require('@codeceptjs/configure');
+const {setHeadlessWhen} = require('@codeceptjs/configure');
 const argv = require('yargs').argv;
 let brow = 'chrome';
 if (argv.browser !== undefined) { //Check browser
-  brow = argv.browser;
+    brow = argv.browser;
 }
 // turn on headless mode when running with HEADLESS=true environment variable
 // HEADLESS=true npx codecept run
 setHeadlessWhen(process.env.HEADLESS);
 
 exports.config = {
-  tests: 'test/*-spec.js',
-  output: './output',
-  helpers: {
-    WebDriver: {
-      url: 'https://www.tut.by/',
-      browser: brow,
-      restart: false,
-      windowSize: "maximize"
-    }
-  },
-  include: {
-    I: './steps_file.js'
-  },
-  bootstrap: null,
-  mocha: {},
-  name: 'codecept',
-  plugins: {
-    retryFailedStep: {
-      enabled: true
+    tests: 'test/*-spec.js',
+    output: './output',
+    helpers: {
+        WebDriver: {
+            url: 'https://www.tut.by/',
+            browser: brow,
+            restart: false,
+            windowSize: "maximize"
+        }
     },
-    wdio: {
-      enabled: true,
-      services: ['selenium-standalone']
+    include: {
+        I: './steps_file.js'
     },
-    screenshotOnFail: {
-      enabled: true
+    bootstrap: null,
+    mocha: {},
+    name: 'codecept',
+    plugins: {
+        retryFailedStep: {
+            enabled: true
+        },
+        wdio: {
+            enabled: true,
+            services: ['selenium-standalone']
+        },
+        screenshotOnFail: {
+            enabled: true
+        },
+        allure: {
+            enabled: true
+        }
     }
-  }
 };
